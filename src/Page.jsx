888d48d30ab18b6da4f769804fc3b1522e4b22f1
shaxzod0022 <ilgeldiyevshaxzod@gmail.com>
2025-01-12 22:16:40 +0500

    completed

--- src/Page.jsx
+++ src/Page.jsx
@@ -17,38 +17,39 @@ import {
   SousPasta,
   XolodnyNapitki,
 } from "./components";
+import { style } from "./utils/styles";
 
 function Page() {
   return (
-    <div className="max-w-[1000px] mx-auto">
+    <div className={`max-w-[1000px] mx-auto ${style.flexCol} items-center`}>
       <Language />
       <Header />
       <Head />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <Salads />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <Appetizers />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <ColdAppetizers />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <Shashlik />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <FirstCourses />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <Garnir />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <Meat />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <JizBiz />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <SousPasta />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <DisertHlep />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <GoryachyNapitki />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <XolodnyNapitki />
-      <hr className="h-[2px] w-full bg-black" />
+      <hr className="h-[2px] w-[92%] bg-blue-950" />
       <Footer />
     </div>
   );
